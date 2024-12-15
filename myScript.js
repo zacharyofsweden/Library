4561f61d54ddcb8714db7c431fe8787873dbcafa
zacharyofsweden <zacharyofsweden@gmail.com>
2024-12-15 06:47:57 +0100

    Working on fixing form logic

--- myScript.js
+++ myScript.js
@@ -1,12 +1,13 @@
 const myLibrary = [];
 
-function Book(title, author, pageCount, Isread, shown) {
+//Todo Make sure that my form gets subbmited to right place in code 
+//Todo Make sure that i can transfer the dom to the book class like author = new Book ("sss" SSS)
+
+function Book(title, author, pageCount) {
     //The Construktor
     this.title = title
     this.author = author
     this.pageCount = pageCount
-    this.Isread = Isread
-    this.shown = shown
 }
 
 
@@ -14,24 +15,55 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-const theHobbit = new Book("Hobbit", "JR Tolken", "222", "true", "true");
+const theHobbit = new Book("Hobbit", "JR Tolken", "222");
 
 addBookToLibrary(theHobbit)
-
+//TODO Change it to use a modol dialog instead much easier. 
 function bookForm() {
+    const formArray = []
     //Basic form container
-    const container = document.querySelector(".container");
-    const Background = document.createElement("div");
-    Background.classList.add("formBackground")
+    const modal = document.querySelector(".modalForm")
+    const openModal = document.querySelector(".addButton")
+    const closeModal = document.querySelector(".modalClose")
+    const authorInput = document.querySelector(".modalClose")
+    const titleInput = document.querySelector(".modalClose")
+    const pageCountInput = document.querySelector(".modalClose")
+
+
+    //Submit logic 
+    //when i submit gather all of their input and add that in a function 
+    //Todo use an array and add all of these in the array
+    //then use array method like reduce to add them all in to a new book
+    //and then add it to library 
+    function handelSubmit() {
+        console.log("working")
+        formArray.push(authorInput.value)
+        formArray.push(titleInput.value)
+        formArray.push(pageCountInput.value)
+    }
 
     //Inputs
+    openModal.addEventListener("click", () => {
+        modal.showModal();
+    });
+
+    closeModal.addEventListener("click", () => {
+        modal.close();
+    })
+
+    closeModal.addEventListener(".submitButton", () => {
+        preventDefault();
+        console.log("working")
+    })
 
 
-    //adds all of it to main node
-    container.appendChild(Background)
 
 }
-bookForm()
+
+
+
+
+
 
 myLibrary.forEach(displayBookShelf)
 
@@ -39,3 +71,5 @@ function displayBookShelf(book) {
     console.log(book)
 
 }
+
+bookForm()
