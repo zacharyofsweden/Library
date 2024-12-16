5aa743d7de26ef7776b265b683f37cb0fbe4d768
zacharyofsweden <zacharyofsweden@gmail.com>
2024-12-16 16:42:57 +0100

    Fixied so you can both add and delete books

--- myScript.js
+++ myScript.js
@@ -43,7 +43,6 @@ function bookForm() {
     //add it to library 
     function handelSubmit() {
         const testBook = new Book(titleInput.value, authorInput.value, pageCountInput.value)
-        console.log(testBook)
         addBookToLibrary(testBook)
         modal.close();
     }
@@ -61,49 +60,69 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
     bookCount++;
     displayBookShelf(myLibrary[myLibrary.length - 1])
+    deleteBook()
 }
 
 
+function deleteBook() {
+    //quarry what book got it's button clicked 
+    //use the data link and remove the book in the array. 
+    //Remove all the dom elements
+    const nodes = document.querySelectorAll(".deleteBookButton")
+    let bookListener = nodes[nodes.length - 1]
+    bookListener.addEventListener("click", (e) => {
+
+        myLibrary.splice(e.target.dataset.bookId, 1)
+        bookListener.parentElement.remove()
+
+    })
+
+
+}
 
 
 
 function displayBookShelf(book) {
     console.log(myLibrary.length)
-   
-   
-        //Increase bookDisplay count to keep track on all the books we track
-      
-        console.log(book)
-        //Gets container to contain the books 
-        const shelf = document.querySelector(".ShelfContainer")
-    
-        //Create a new book div
-        const newBook = document.createElement("div")
-        newBook.classList.add("bookTest");
-    
-    
-        //Create new paragraph for all the info
-        const titleInfo = document.createElement("p")
-        titleInfo.classList.add("titleStyle")
-        const authorInfo = document.createElement("p")
-        authorInfo.classList.add("authorStyle")
-        const pageCountInfo = document.createElement("p")
-        pageCountInfo.classList.add("pagesCountStyle")
 
-        //displays the book attributes  
-        titleInfo.innerHTML = (book.title)
-        authorInfo.innerHTML = (book.author)
-        pageCountInfo.innerHTML = (book.pageCount)
+    //Increase bookDisplay count to keep track on all the books we track
 
-        //add all of this too the dom 
-        newBook.appendChild(titleInfo)
-        newBook.appendChild(authorInfo)
-        newBook.appendChild(pageCountInfo)
-        shelf.appendChild(newBook)
-        bookDisplayCount++;
-       
- 
-   
-    
+    console.log(book)
+    //Gets container to contain the books 
+    const shelf = document.querySelector(".ShelfContainer")
+
+    //Create a new book div
+    const newBook = document.createElement("div")
+    newBook.classList.add("bookTest");
+
+
+    //Create new paragraph for all the info
+    const titleInfo = document.createElement("p")
+    titleInfo.classList.add("titleStyle")
+    const authorInfo = document.createElement("p")
+    authorInfo.classList.add("authorStyle")
+    const pageCountInfo = document.createElement("p")
+    pageCountInfo.classList.add("pagesCountStyle")
+    const deleteBookButton = document.createElement("button")
+    deleteBookButton.classList.add("deleteBookButton")
+
+    //displays the book attributes  
+    titleInfo.innerHTML = (book.title)
+    authorInfo.innerHTML = (book.author)
+    pageCountInfo.innerHTML = (book.pageCount)
+    deleteBookButton.innerHTML = ("delete")
+    deleteBookButton.dataset.bookId = myLibrary.length - 1
+
+    //add all of this too the dom 
+    newBook.appendChild(titleInfo)
+    newBook.appendChild(authorInfo)
+    newBook.appendChild(pageCountInfo)
+    newBook.appendChild(deleteBookButton)
+    shelf.appendChild(newBook)
+    bookDisplayCount++;
+
+
+
+
 }
 bookForm()
